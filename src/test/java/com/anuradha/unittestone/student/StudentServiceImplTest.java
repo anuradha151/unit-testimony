7aa7fd701354e75cc7b49e7a2fa23fe3b75f2643
Anuradha Ranasinghe <anuradha@arimaclanka.com>
2023-01-07 19:02:09 +0530

    Feature : unit testing save and find all

--- src/test/java/com/anuradha/unittestone/student/StudentServiceImplTest.java
+++ src/test/java/com/anuradha/unittestone/student/StudentServiceImplTest.java
@@ -5,17 +5,18 @@ import com.anuradha.unittestone.enums.Gender;
 import com.anuradha.unittestone.repo.StudentRepository;
 import com.anuradha.unittestone.service.StudentService;
 import com.anuradha.unittestone.service.impl.StudentServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.web.server.ResponseStatusException;
 
-
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,17 +45,15 @@ class StudentServiceImplTest {
         // given
         Student student = new Student("Mahinda Rajapaksa", "hora.gediya@gmail.com", Gender.MALE);
 
+        BDDMockito.given(studentRepository.selectExistsEmail(anyString())).willReturn(true);
         // when
-        studentService.save(student);
-
         // then
-        ArgumentCaptor<Student> argumentCaptor = ArgumentCaptor.forClass(Student.class);
+        Assertions.assertThatThrownBy(()-> studentService.save(student))
+                .isInstanceOf(ResponseStatusException.class)
+                .hasMessageContaining("Existing email address. Please use another");
 
-        verify(studentRepository).save(argumentCaptor.capture());
 
-        Student value = argumentCaptor.getValue();
 
-        assertThat(value).isEqualTo(student);
 
 
     }
